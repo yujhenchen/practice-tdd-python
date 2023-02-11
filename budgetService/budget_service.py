6c5852f4d544735530fd2095337def2232be39b1
yujhenchen <yujhen.chen93@gmail.com>
2023-02-11 17:35:31 +0800

    add month test case

--- budgetService/budget_service.py
+++ budgetService/budget_service.py
@@ -9,10 +9,20 @@ class BudgetService:
         budgetLs = self.budgetRepo.getAll()
         if len(budgetLs) > 0:
             startYM = start.strftime("%Y%m")
-            amount = 0
-            for b in budgetLs:
-                if b.yearMonth == startYM:
-                    amount = b.amount
-                    break
-            return amount / 31
+            endYM = end.strftime("%Y%m")
+            amount = budgetLs[0].amount
+            diff_day = (end - start).days + 1
+
+            if budgetLs[0].yearMonth == startYM and diff_day == 31:
+                return amount
+            if budgetLs[0].yearMonth == startYM:
+                return amount / 31
+            # amount = 0
+            # for b in budgetLs:
+
+            #     if b.yearMonth == startYM:
+            #         amount = b.amount
+            #         break
+            # month_days =
+            # return amount / 31
         return 0.0
--- budgetService/budget_service_test.py
+++ budgetService/budget_service_test.py
@@ -20,6 +20,27 @@ class BudgetTestCase(unittest.TestCase):
         end = date.fromisoformat("2019-12-04")
         self.assertEqual(budgetService.query(start, end), 100)
 
+    # def test_two_day_has_budget_within(self):
+    #     budgetRepo = IBudgetRepo([Budget("201912", 3100)])
+    #     budgetService = BudgetService(budgetRepo)
+    #     start = date.fromisoformat("2019-12-04")
+    #     end = date.fromisoformat("2019-12-05")
+    #     self.assertEqual(budgetService.query(start, end), 200)
+
+    def test_one_month_has_budget_within(self):
+        budgetRepo = IBudgetRepo([Budget("201912", 3100)])
+        budgetService = BudgetService(budgetRepo)
+        start = date.fromisoformat("2019-12-01")
+        end = date.fromisoformat("2019-12-31")
+        self.assertEqual(budgetService.query(start, end), 3100)
+
+    # def test_cross_month_has_budget_within(self):
+    #     budgetRepo = IBudgetRepo([Budget("201911", 30000),Budget("201912", 3100)])
+    #     budgetService = BudgetService(budgetRepo)
+    #     start = date.fromisoformat("2019-11-29")
+    #     end = date.fromisoformat("2019-12-03")
+    #     self.assertEqual(budgetService.query(start, end), 3100)
+
 
 if __name__ == "__main__":
     unittest.main()
