7891dd705feb52a5fcb165d9b78b33b6eeb8916c
yujhenchen <yujhen.chen93@gmail.com>
2023-02-14 14:58:04 +0800

    add fail first test case

--- tennis/tennis.py
+++ tennis/tennis.py
@@ -1,3 +1,6 @@
 class Tennis:
     def __init__(self) -> None:
         pass
+
+    def get_result(self) -> str:
+        return ""
--- tennis/tennis_test.py
+++ tennis/tennis_test.py
@@ -1,10 +1,12 @@
 import unittest
-
+from tennis import Tennis
 
 class TennisTestCase(unittest.TestCase):
     def test_love_all(self) -> None:
-        pass
+        tennis = Tennis()
+        self.assertEqual('love all', tennis.get_result())
 
 
 if __name__ == "__main__":
+    unittest.main()
     pass
