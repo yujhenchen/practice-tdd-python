76012069aa19cb871e49b66310265bff0acd4033
yujhenchen <yujhen.chen93@gmail.com>
2023-02-14 15:11:27 +0800

    add domain knowledge codes, player1 and player2 score

--- tennis/tennis.py
+++ tennis/tennis.py
@@ -2,5 +2,5 @@ class Tennis:
     def __init__(self) -> None:
         pass
 
-    def get_result(self) -> str:
+    def get_result(self, player1Scores, player2Scores) -> str:
         return 'love all'
