2d8597c08ce0c09ff7e0561bcee68c27fe9acb7e
yujhenchen <yujhen.chen93@gmail.com>
2023-02-14 15:13:02 +0800

    add domain knowledge codes, player1 and player2 score

--- tennis/tennis_test.py
+++ tennis/tennis_test.py
@@ -1,10 +1,11 @@
 import unittest
 from tennis import Tennis
 
+
 class TennisTestCase(unittest.TestCase):
     def test_love_all(self) -> None:
         tennis = Tennis()
-        self.assertEqual('love all', tennis.get_result())
+        self.assertEqual("love all", tennis.get_result(0, 0))
 
 
 if __name__ == "__main__":
