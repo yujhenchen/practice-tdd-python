6a9f13ae6aa0143d16be4262f9222b9f43eee38b
yujhenchen <yujhen.chen93@gmail.com>
2023-02-14 15:32:34 +0800

    change codes to pass test

--- tennis/tennis.py
+++ tennis/tennis.py
@@ -1,6 +1,12 @@
 class Tennis:
     def __init__(self) -> None:
-        pass
+        self.scoreMap = {0: "love", 1: "fifteen"}
 
     def get_result(self, player1Scores, player2Scores) -> str:
-        return 'love all'
+        if player1Scores == player2Scores:
+            return "love all"
+
+        return "{scoreStr1} {scoreStr2}".format(
+            scoreStr1=self.scoreMap[player1Scores],
+            scoreStr2=self.scoreMap[player2Scores],
+        )
